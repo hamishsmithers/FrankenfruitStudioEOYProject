7297fea0bf2bed0e6f819dff0842a7b951a5e231
NerbzHub <nathan.nette@gmail.com>
2017-09-22 09:48:17 +1000

    ALPHA1.01
    Changes:
    
    Added keyboard movement for the character. WASD to move. 
    The character also is always facing the mouse's position.
    Left click is shoot. The character shoots Tennisball.prefab, they spawn onClick.
    Added non-rendering box colliders around the arena border.
    Added a rigidBody to Tennisball.prefab and character prefab.
    
    Scripts added:
    
    Scripts/PlayerMovement.cs
    Scripts/Shoot.cs

---
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/Shoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour {
+  
+    public GameObject m_TennisBall = null;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            GameObject copy = Instantiate(m_TennisBall);
+            copy.transform.position = transform.position + transform.forward;
+
+            Rigidbody rb = copy.GetComponent<Rigidbody>();
+            rb.AddForce(transform.forward * 1000, ForceMode.Acceleration);
+            
+        }
+    }
+}
