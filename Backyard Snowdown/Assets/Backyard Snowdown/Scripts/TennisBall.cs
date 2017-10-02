b70ccba867dbdd5c8d4fe1ac2f6479bfa1569983
RvBVakama <RvBVakama@users.noreply.github.com>
2017-10-02 23:35:16 +1100

    ALPHA1.14d Tinkering.

--- Backyard Snowdown/Assets/Backyard Snowdown/Scripts/TennisBall.cs
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/TennisBall.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class TennisBall : MonoBehaviour
 {
     Rigidbody rb;
-
+    private float fVelCount = 0.0f;
     //-------
     // Score
     //-------
@@ -20,18 +20,71 @@ public class TennisBall : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(rb.velocity);
+        //Debug.Log(rb.velocity);
 
-        if (rb.velocity.x < 2.0f || rb.velocity.x < -2.0f)
+        //if (fVelCount > 1.0f || Input.GetKeyDown(KeyCode.Mouse0))
+        //{
+        //    fVelCount += Time.deltaTime;
+        //    Debug.Log("ACTIVE");
+        //}
+        //else
+        //{
+        //    if (rb.velocity.x <= 0.5f && fVelCount != 0)
+        //    {
+        //        rb.velocity = Vector3.zero;
+        //        fVelCount = 0.0f;
+        //    }
+
+        //    if (rb.velocity.y <= 0.5f && fVelCount != 0)
+        //    {
+        //        rb.velocity = Vector3.zero;
+        //        fVelCount = 0.0f;
+        //    }
+        //}
+
+        if (rb.velocity.x < 2.0f)
             nScoreValue = 0;
-        else
-            nScoreValue = 2;
-        
-        if (rb.velocity.z < 2.0f || rb.velocity.z < -2.0f)
+
+        if (rb.velocity.y < 2.0f)
             nScoreValue = 0;
-        else
+
+        if (rb.velocity.x > 2.0f)
             nScoreValue = 2;
 
-        Debug.Log(nScoreValue);
+        if (rb.velocity.y > 2.0f)
+            nScoreValue = 2;
+
+        if (rb.velocity.x > -2.0f)
+            nScoreValue = 2;
+
+        if (rb.velocity.y > -2.0f)
+            nScoreValue = 2;
+
+        //if (rb.velocity.x < 2.0f && rb.velocity.x > 0.0f || rb.velocity.x > -2.0f && rb.velocity.x < 0.0f || rb.velocity.z < 2.0f && rb.velocity.z > 0.0f || rb.velocity.z > -2.0f && rb.velocity.z < 0.0f)
+        //    nScoreValue = 2;
+        //else if (rb.velocity == Vector3.zero)
+        //    nScoreValue = 0;
+
+        //if (rb.velocity.x < 2.0f && rb.velocity.x > 0.0f && rb.velocity.z == 0.0f)
+        //    nScoreValue = 2;
+        //else if (rb.velocity == Vector3.zero)
+        //    nScoreValue = 0;
+
+        //if (rb.velocity.z < 2.0f && rb.velocity.z > 0.0f && rb.velocity.x == 0.0f)
+        //    nScoreValue = 2;
+        //else if (rb.velocity == Vector3.zero)
+        //    nScoreValue = 0;
+
+        //if (rb.velocity.x > -2.0f && rb.velocity.x < 0.0f && rb.velocity.z == 0.0f)
+        //    nScoreValue = 2;
+        //else if (rb.velocity == Vector3.zero)
+        //    nScoreValue = 0;
+
+        //if (rb.velocity.z > -2.0f && rb.velocity.z < 0.0f && rb.velocity.x == 0.0f)
+        //    nScoreValue = 2;
+        //else if (rb.velocity == Vector3.zero)
+        //    nScoreValue = 0;
+
+        //Debug.Log(nScoreValue);
     }
 }
