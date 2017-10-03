1e9ba6886335ca2a21f4c72d30f9c5174237f4d5
NerbzHub <nathan.nette@gmail.com>
2017-10-03 12:33:03 +1100

    ALPHA1.17
    Added:
    ClothesLine.fbx
    Weapon.fbx
    Kennel.fbx
    SnowMan.fbx
    And basic materials on them.
    
    Modified:
    Main.unity

---
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/AbilitySnowMan.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilitySnowMan : MonoBehaviour
+{
+
+    // Counts how many times it's been hit
+    public int nSnowManHitCount = 2;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nSnowManHitCount < 1)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.tag == "TennisBall")
+        {
+            nSnowManHitCount = nSnowManHitCount - 1;
+        }
+        
+    }
+}
