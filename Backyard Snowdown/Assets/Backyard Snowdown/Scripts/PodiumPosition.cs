f30908befae6e4fa2f4801d86a3397843708b462
NerbzHub <nathan.nette@gmail.com>
2017-10-08 01:02:58 +1100

    ALPHA1.22
    Added:
    EndRound.unity
    PodiumPosition.cs

---
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/PodiumPosition.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PodiumPosition : MonoBehaviour {
+
+	// Use this for initialization
+	void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    //------------------------------------------------
+    // Implementation
+    //------------------------------------------------
+
+    // Create a list that holds which player got which spot. 
+    // When this scene loads, it puts the players in the different podium spawnboxes.
+}
