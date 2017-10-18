b0480dc3ce635652453f34686812787c2551f48d
RvBVakama <RvBVakama@users.noreply.github.com>
2017-10-18 11:48:24 +1100

    ALPHA1.33
    Commit before Alpha Build.

---
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/PodiumButtonAction.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using XboxCtrlrInput;		// Be sure to include this if you want an object to have Xbox input
+
+public class PodiumButtonAction : MonoBehaviour {
+
+    public XboxController controller;
+
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (XCI.GetButtonDown(XboxButton.Start, controller) || XCI.GetButton(XboxButton.A, controller))
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
