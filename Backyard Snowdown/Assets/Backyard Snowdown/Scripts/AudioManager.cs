0929bba08a1cf8dce1754cdbb2b66adba68ecfc5
NerbzHub <nathan.nette@gmail.com>
2017-11-27 09:45:07 +1100

    GOLD1.12d Fixed: SFX audio slider in main menu not applying the volume change to the main default.

--- Backyard Snowdown/Assets/Backyard Snowdown/Scripts/AudioManager.cs
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/AudioManager.cs
@@ -38,89 +38,71 @@ public class AudioManager : MonoBehaviour {
 	void Update ()
     {
         sfxSource.volume = Global.SFXVolume;
-        //if (scpPlayer.m_bThrowBall)
-        //{
-        //    int Selector = Random.Range(0, sfxThrow.Length);
-        //    sfxSource.clip = sfxThrow[Selector];
-        //    sfxSource.Play();
-        //}
-
-        //if (scpDash.m_bDashing == true)
-        //{
-        //    PlayDashAudio();
-        //}
     }
 
     public void PlayDashAudio()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxDash;
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxDash;
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlayThrowAudio()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
         int Selector = Random.Range(0, sfxThrow.Length);
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxThrow[Selector];
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxThrow[Selector];
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlaySnowmanBoingAudio()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxSnowManBoing;
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxSnowManBoing;
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlaySnowBallHit()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
         int Selector = Random.Range(0, sfxHitArray.Length);
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxHitArray[Selector];
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxHitArray[Selector];
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlayHurtAudio()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
         int Selector = Random.Range(0, sfxHurtArray.Length);
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxHurtArray[Selector];
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxHurtArray[Selector];
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlaySnowmanCrumble()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxSnowManCrumble;
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxSnowManCrumble;
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
     public void PlaySnowmanSummon()
     {
         GameObject sound = Instantiate(goSound);
-        AudioSource audSource = sound.GetComponent<AudioSource>();
-        audSource.outputAudioMixerGroup = m_audmixMixer;
-        audSource.clip = sfxSnowManSummon;
-        audSource.Play();
-        audSource.pitch = Random.Range(0.9f, 1.1f);
+        sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        sfxSource.clip = sfxSnowManSummon;
+        sfxSource.Play();
+        sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 }
