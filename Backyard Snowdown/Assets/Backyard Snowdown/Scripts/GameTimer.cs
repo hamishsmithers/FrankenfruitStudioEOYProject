a0ee51cd5c1b9ca0408a47271f2dc6176f9d2ba9
HamishSmithers <hamish.e.smithers@gmail.com>
2017-11-26 21:57:33 +1100

    GOLD1.12c Changed the way the time is counted/displayed.

--- Backyard Snowdown/Assets/Backyard Snowdown/Scripts/GameTimer.cs
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/GameTimer.cs
@@ -8,6 +8,10 @@ public class GameTimer : MonoBehaviour
 {
     public Text m_txtGameTime;
     private float m_fGameTime = 0.0f;
+
+	private int m_nSeconds=0;
+	private int m_nMinutes=0;
+	private float initialTime;
     
     // Next update in second
     private int nextUpdate = 1;
@@ -22,17 +26,35 @@ public class GameTimer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        string strMinutes = ((int)m_fGameTime / 60).ToString();
-        string strSeconds = (m_fGameTime % 60).ToString("f0");
-        m_txtGameTime.text = strMinutes + ":" + strSeconds;
+		m_fGameTime = Time.timeSinceLevelLoad;
+		m_nSeconds = (int)m_fGameTime;
+		m_nSeconds -= m_nMinutes * 60;
+
+		if (m_nSeconds == 60) {
+			m_nMinutes++;
+			m_nSeconds = 0;
+		}
+
+		if (m_nSeconds < 10) {
+			m_txtGameTime.text = m_nMinutes + ":0" + m_nSeconds;
+		} else {
+			m_txtGameTime.text = m_nMinutes + ":" + m_nSeconds;
+		}
+
+
+
+//        string strMinutes = ((int)m_fGameTime / 60).ToString();
+//        string strSeconds = (m_fGameTime % 60).ToString("f0");
+//        m_txtGameTime.text = strMinutes + ":" + strSeconds;
 
         // breaks when ticking over each minute
-        if (m_fGameTime % 60 > 9.5f)
-            m_txtGameTime.text = strMinutes + ":" + strSeconds;
-        else
-            m_txtGameTime.text = strMinutes + ":0" + strSeconds;
-
-        m_fGameTime += Time.deltaTime;
+//        if (m_fGameTime % 60 > 9.5f)
+//            m_txtGameTime.text = strMinutes + ":" + strSeconds;
+//        else
+//            m_txtGameTime.text = strMinutes + ":0" + strSeconds;
+//		m_txtGameTime.text=Time.time.ToString();
+//
+//		m_fGameTime = Time.time;
         
         //// If the next update is reached
         //if (Time.time >= nextUpdate)
