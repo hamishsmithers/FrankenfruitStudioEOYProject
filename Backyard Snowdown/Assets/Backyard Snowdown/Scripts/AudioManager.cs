607e601997c7132506a16558cf42196718827c58
NerbzHub <nathan.nette@gmail.com>
2017-11-27 22:14:39 +1100

    GOLD1.16 Commenting update Modified AudioManager.cs.

--- Backyard Snowdown/Assets/Backyard Snowdown/Scripts/AudioManager.cs
+++ Backyard Snowdown/Assets/Backyard Snowdown/Scripts/AudioManager.cs
@@ -41,121 +41,196 @@ public class AudioManager : MonoBehaviour {
     //--------------------------------------
     // Sound Effect for Sounds
     //--------------------------------------
-    [LabelOverride("GameObject Sound")]
+    [LabelOverride("Sound GameObject")]
     [Tooltip("")]
     public GameObject goSound = null;
     //--------------------------------------
     // Sound Effect for SFX Source
     //--------------------------------------
-    [LabelOverride("")]
+    [LabelOverride("SFX Audio Source")]
     [Tooltip("")]
     public AudioSource sfxSource = null;
     //--------------------------------------
-    // Sound Effect for Dash
+    // Sound Effect for Throw
     //--------------------------------------
     [LabelOverride("")]
     [Tooltip("")]
     public AudioClip[] sfxThrow = null;
-    //--------------------------------------
-    // Sound Effect for Dash
-    //--------------------------------------
+    //----------------------------------------------------
+    // Array of AudioClips storing the varied hit sounds.
+    //----------------------------------------------------
     [LabelOverride("")]
     [Tooltip("")]
     public AudioClip[] sfxHitArray = null;
-    //--------------------------------------
-    // Sound Effect for Dash
-    //--------------------------------------
+    //-----------------------------------------------------
+    // Array of AudioClips storing the varied hurt sounds.
+    //-----------------------------------------------------
     [LabelOverride("")]
     [Tooltip("")]
     public AudioClip[] sfxHurtArray = null;
 
-
-
+    //-------------------------------------------------------------------------------
+    // Creating a new instance of the Player class to reference parts of the script.
+    //-------------------------------------------------------------------------------
     private Player scpPlayer;
+    //-------------------------------------------------------------------------------
+    // Creating a new instance of the Player class to reference parts of the script.
+    //-------------------------------------------------------------------------------
     private Dash scpDash;
 
+    //---------------------------
+    // Awake is a Unity Function
+    //---------------------------
     void Awake()
     {
         m_SharedInstance = this;
     }
 
+    //-----------------------------
     // Use this for initialization
+    //-----------------------------
     void Start ()
     {
+        // Allocates the AudioSource class into the audio source called sfxSource
         sfxSource = gameObject.GetComponent<AudioSource>();
+
+        // Allocates the Player class into the scpPlayer to get the script and be able to use the script in this code.
         scpPlayer = gameObject.GetComponent<Player>();
+
+        // Allocates the Dash class into the scpDash to get the script and be able to use the script in this code.
         scpDash = gameObject.GetComponent<Dash>();
     }
 	
+    //---------------------------------
 	// Update is called once per frame
+    //---------------------------------
 	void Update ()
     {
         sfxSource.volume = Global.SFXVolume;
     }
 
+    //------------------------------------------------------------
+    // PlayDashAudio is the function called when a player dashes. 
+    //------------------------------------------------------------
     public void PlayDashAudio()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to the dash sound.
         sfxSource.clip = sfxDash;
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //------------------------------------------------------------------------
+    // PlayThrowAudio is the function called when a player throws a snowball. 
+    //------------------------------------------------------------------------
     public void PlayThrowAudio()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // Creates an int that is randomised between 0 and the length of the throw array.
         int Selector = Random.Range(0, sfxThrow.Length);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to a randomized index in the throw array.
         sfxSource.clip = sfxThrow[Selector];
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //------------------------------------------------------------------------
+    // PlaySnowmanBoingAudio is the function called when the snowman jumps.
+    //------------------------------------------------------------------------
     public void PlaySnowmanBoingAudio()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to the snowman boing sound.
         sfxSource.clip = sfxSnowManBoing;
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //--------------------------------------------------------------------------
+    // PlaySnowBallHit is the function called when the snowball hits something.
+    //--------------------------------------------------------------------------
     public void PlaySnowBallHit()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // Creates an int that is randomised between 0 and the length of the hit array.
         int Selector = Random.Range(0, sfxHitArray.Length);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to a randomized index in the hit array.
         sfxSource.clip = sfxHitArray[Selector];
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //--------------------------------------------------------------------
+    // PlayHurtAudio is the function called when the player loses health.
+    //--------------------------------------------------------------------
     public void PlayHurtAudio()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // Creates an int that is randomised between 0 and the length of the hurt array.
         int Selector = Random.Range(0, sfxHurtArray.Length);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to a randomized index in the hurt array.
         sfxSource.clip = sfxHurtArray[Selector];
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //-----------------------------------------------------------------
+    // PlaySnowmanCrumble is the function called when the snowman dies
+    //-----------------------------------------------------------------
     public void PlaySnowmanCrumble()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to the snowman crumble sound effect.
         sfxSource.clip = sfxSnowManCrumble;
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 
+    //-----------------------------------------------------------------------
+    // PlaySnowmanSummon is the function called when the snowman is summoned
+    //-----------------------------------------------------------------------
     public void PlaySnowmanSummon()
     {
+        // Instantiates the sound GameObject.
         GameObject sound = Instantiate(goSound);
+        // The sound effect source's mixer group is now the same as the m_audMixMixer.
         sfxSource.outputAudioMixerGroup = m_audmixMixer;
+        // Sets the source's clip to the snowman summon sound effect.
         sfxSource.clip = sfxSnowManSummon;
+        // Play the current clip.
         sfxSource.Play();
+        // This varies the pitch randomly to emulate different clips.
         sfxSource.pitch = Random.Range(0.9f, 1.1f);
     }
 }
